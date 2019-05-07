5b4deb6c557227abb007a2dea9b44d6019d00b13
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-07 15:50:52 +0300

    cors added

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(bodyParser.json())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
+
+app.use(cors())
 
 let persons = [
   {
