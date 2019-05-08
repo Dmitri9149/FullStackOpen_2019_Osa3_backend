9bbf160d89daac2cccbd7a04660893990ad67572
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-08 16:07:00 +0300

    build is added

---
+++ build/precache-manifest.6792c6c14f3d2439119b991041bc32f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "313d7dffde4e5e81af40",
+    "url": "/static/js/main.13ab644e.chunk.js"
+  },
+  {
+    "revision": "717095a3ea21a6bbdee1",
+    "url": "/static/js/2.3d2452bb.chunk.js"
+  },
+  {
+    "revision": "313d7dffde4e5e81af40",
+    "url": "/static/css/main.febdce7f.chunk.css"
+  },
+  {
+    "revision": "8baaae848cd10ec837e79f4478a9f62b",
+    "url": "/index.html"
+  }
+];
