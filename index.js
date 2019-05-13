1f86a1922817e908cd587b3bf86974db6427e6c1
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-13 13:26:39 +0300

    app.post if condition is changed to body.name === undefined

--- index.js
+++ index.js
@@ -58,9 +58,9 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name) {
+  if (body.name === undefined) {
     return response.status(400).json({error: 'name = undefined'})
-  } else if (!body.number) {
+  } else if (body.number === undefined) {
     return response.status(400).json({error:"number = undefined"})
   }  
 
